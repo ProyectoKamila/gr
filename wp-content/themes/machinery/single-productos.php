8788d54a35db31706bfa0752f1f6bb0d1d7549f3
maikolleon20151 <maikol.leon@proyectokamila.com>
2015-10-21 15:51:05 +0000

    arreglos

--- wp-content/themes/machinery/single-productos.php
+++ wp-content/themes/machinery/single-productos.php
@@ -362,7 +362,7 @@
                              </li>
                               <li onclick="mostrarvideos();"   class="act act4 col-lg-3 col-md-3 col-sm-6 col-xs-12">
                                  <a class="hvr-wobble-horizontal">
-                                    Videos
+                                    Estado Actual
                                  </a>
                              </li>
                         </ul>
